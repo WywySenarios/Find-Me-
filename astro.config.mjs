82922d29861fe38f6a69d40fe840d45217647384
WywySenarios <77202595+WywySenarios@users.noreply.github.com>
2025-03-16 07:52:04 -0400

    Add Vercel Adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,34 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
 // import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	devToolbar: {
-		enabled: false
-	}
+  // integrations: [
+  // 	starlight({
+  // 		title: 'My Docs',
+  // 		social: {
+  // 			github: 'https://github.com/withastro/starlight',
+  // 		},
+  // 		sidebar: [
+  // 			{
+  // 				label: 'Guides',
+  // 				items: [
+  // 					// Each item here is one entry in the navigation menu.
+  // 					{ label: 'Example Guide', slug: 'guides/example' },
+  // 				],
+  // 			},
+  // 			{
+  // 				label: 'Reference',
+  // 				autogenerate: { directory: 'reference' },
+  // 			},
+  // 		],
+  // 	}),
+  // ],
+  devToolbar: {
+      enabled: false
+	},
 
-	// integrations: [
-	// 	starlight({
-	// 		title: 'My Docs',
-	// 		social: {
-	// 			github: 'https://github.com/withastro/starlight',
-	// 		},
-	// 		sidebar: [
-	// 			{
-	// 				label: 'Guides',
-	// 				items: [
-	// 					// Each item here is one entry in the navigation menu.
-	// 					{ label: 'Example Guide', slug: 'guides/example' },
-	// 				],
-	// 			},
-	// 			{
-	// 				label: 'Reference',
-	// 				autogenerate: { directory: 'reference' },
-	// 			},
-	// 		],
-	// 	}),
-	// ],
+  adapter: vercel(),
 });
