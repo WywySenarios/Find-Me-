f1c7d5f0384048aafecd4f618c0453781421d17e
WywySenarios <77202595+WywySenarios@users.noreply.github.com>
2025-03-15 23:18:49 -0400

    Levels 1-3
    We got the basics down...

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,10 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+	devToolbar: {
+		enabled: false
+	}
+
 	// integrations: [
 	// 	starlight({
 	// 		title: 'My Docs',
